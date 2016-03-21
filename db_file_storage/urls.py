5841732fc40b0fb6e2edeb284066e4b527efae79
wszyszka <winicjusz.szyszka@comp.com.pl>
2016-03-21 12:25:59 +0100

    Usuniete kolejne warningi

--- db_file_storage/urls.py
+++ db_file_storage/urls.py
@@ -1,21 +1,20 @@
 # django
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from db_file_storage import views
 
 
-urlpatterns = patterns(
-    'db_file_storage',
+urlpatterns = [
     url(
-        r'^download/',
+        r'^db_file_storage/download/',
         views.get_file,
         {'add_attachment_headers': True},
         name='db_file_storage.download_file'
     ),
     url(
-        r'^get/',
+        r'^db_file_storage/get/',
         views.get_file,
         {'add_attachment_headers': False},
         name='db_file_storage.get_file'
     )
-)
+]
